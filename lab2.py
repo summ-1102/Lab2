6623d1c8c5d0106d2edc0ef48135493a832d6d88
SuMyatMon <sumyat.23@ichat.sp.edu.sg>
2024-10-23 18:54:35 +0800

    Add lab2.py

--- lab2.py
+++ lab2.py
@@ -1,1 +1,28 @@
 print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+
+def display_main_menu():
+    print("Enter some numbers separated by commas")
+
+def get_user_input():
+    number_string = input()
+    number_list= number_string.split(",")
+    numbers= [float(num) for num in number_list]
+    print(numbers)
+    return numbers
+
+def calc_average(numbers):
+    average= sum(numbers) / len(numbers)
+    print("Average is " + str(average))
+
+def find_min_max(numbers):
+    numbers.sort()
+    min = numbers[0]
+    max = numbers[len(numbers)-1]
+    print("Min num is " + str(min))
+    print("Max num is " + str(max))
+ 
+display_main_menu()
+number = get_user_input()
+calc_average(number)
+find_min_max(number)
+
